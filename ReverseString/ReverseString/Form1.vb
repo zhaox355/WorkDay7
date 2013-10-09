219b5fbbed83e6c00ef335636a4025e6e8117852
idsc3101 <idsc3101@gmail.com>
2013-10-09 13:22:49 -0500

    Added skeleton project

---
+++ ReverseString/ReverseString/Form1.vb
@@ -0,0 +1,19 @@
+﻿Option Strict On
+
+Public Class Form1
+
+    Private Sub btnStart_Click(sender As System.Object, e As System.EventArgs) Handles btnStart.Click
+        ' Read the input from the user
+        Dim st As String
+        st = InputBox("Please type the initial string")
+
+        ' Compute the result
+        Dim result As String = ""
+
+
+
+        ' Report the output to the user
+        MessageBox.Show("Not implemented yet", "Result")
+
+    End Sub
+End Class
