2e2748af4b6d634b02383d4e0e91ac298d232fa3
idsc3101 <idsc3101@gmail.com>
2013-10-14 15:25:56 -0500

    ziyi zhao

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -10,10 +10,18 @@ Public Class Form1
         ' Compute the result
         Dim result As String = ""
 
+        Dim i As Integer
+        For i = st.Length - 1 To 0 Step -1
+            Dim c As Char
+            c = st.Chars(i)
+            result = result & c
+
+        Next
+
 
 
         ' Report the output to the user
-        MessageBox.Show("Not implemented yet", "Result")
+        MessageBox.Show(result, "Result")
 
     End Sub
 End Class
